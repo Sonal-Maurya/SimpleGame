84628a90a26773aa6ce7284958b29219cae79ace
Sonal Maurya <sonalmaury.17@gmail.com>
2022-01-31 14:06:53 +0530

    Initial commit

---
+++ src/com/sonal/Main.java
@@ -0,0 +1,19 @@
+package com.sonal;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String name;
+        String weapon;
+        int health;
+        Player1 player = new Player1(name =" Sonal", weapon = "sword", health= 100);
+
+       player.damageBYGun1();
+        player.damageBYGun1();
+        player.heal();
+        player.damageByGun2();
+
+
+
+    }
+}
